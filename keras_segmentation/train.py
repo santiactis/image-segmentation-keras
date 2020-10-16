16f947664b0ac2bbe9225b004e5eeb8141cf603f
santiactis <67348166+santiactis@users.noreply.github.com>
2020-10-15 21:07:22 -0300

    Update train.py

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -122,7 +122,7 @@ def train(model,
         if ignore_zero_class:
             loss_k = masked_categorical_crossentropy
         else:
-            loss_k = dice_loss
+            loss_k = 'categorical_crossentropy'
 
         model.compile(loss=loss_k,
                       optimizer=optimizer_name,
